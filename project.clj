3bb420f8b9347aadf8d730dca31e1effeea81b42
Hoàng Minh Thắng <p@banphim.net>
2014-11-11 11:00:46 +0700

    add :license to project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
 (defproject fence "0.1.0"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :description "Effortless way to avoid manual extern files when doing
   javascript interop from Clojurescript."
   :url "http://github.com/myguidingstar/fence"
