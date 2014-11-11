ff6cef69e2f60758b84e52b7df8d29f5cb09c904
Hoàng Minh Thắng <p@banphim.net>
2014-11-11 10:53:58 +0700

    first code commit

---
+++ src/fence/core.clj
@@ -0,0 +1,33 @@
+(ns fence.core
+  (:refer-clojure :exclude [..]))
+
+(defmacro dot
+  "Alternative macro version of Clojurescript special form `.`. Will
+  expand to `aget` forms which are resistant to *renaming symbols* feature
+  of Google Closure compiler in `advanced optimizations` level."
+  ([obj attr|method]
+     (cond
+      (list? attr|method)
+      `(dot ~obj ~(first attr|method) ~@(rest attr|method))
+      (symbol? attr|method)
+      (let [the-name (name attr|method)]
+        (if (.startsWith the-name "-")
+          `(aget ~obj ~(apply str (rest (seq the-name))))
+          `(let [obj# ~obj
+                 method# (aget obj# ~the-name)]
+             (.call method# obj#))))
+      :else
+      (throw (Exception. "Invalid dot form."))))
+  ([obj method & args]
+     (let [the-name (name method)]
+       (if (.startsWith the-name "-")
+         (throw (Exception. "Invalid dot form."))
+         `(let [obj# ~obj
+                method# (aget obj# ~the-name)]
+            (.call method# obj# ~@args))))))
+
+(defmacro ..
+  "Effortless dot form macro. Use it instead of core library's `..`
+  macro to avoid manual extern files."
+  ([x form] `(dot ~x ~form))
+  ([x form & more] `(.. (dot ~x ~form) ~@more)))
