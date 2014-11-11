fe3d61511ddcccec72a31092542a2624dbf6bd7b
Hoàng Minh Thắng <p@banphim.net>
2014-11-11 10:53:44 +0700

    add project.clj

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject fence "0.1.0"
+  :description "Effortless way to avoid manual extern files when doing
+  javascript interop from Clojurescript."
+  :url "http://github.com/myguidingstar/fence"
+  :dependencies [[org.clojure/clojure "1.6.0"]])
