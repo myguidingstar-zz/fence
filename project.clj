747948c1cd4e6a58e6e9068284107ddf2a5882ca
Hoàng Minh Thắng <p@banphim.net>
2014-12-03 12:44:56 +0700

    add core.match as project deps

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
-(defproject fence "0.1.0"
+(defproject fence "0.2.0"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :description "Effortless way to avoid manual extern files when doing
   javascript interop from Clojurescript."
   :url "http://github.com/myguidingstar/fence"
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/core.match "0.2.2"]])
